867a8b6011a8d7a72ba6acaa7b494680e31eeddd
Brian Santoso <brian.santoso@berkeley.edu>
2021-04-07 15:51:56 -0700

    set up webpack live dev server

--- public/README.md
+++ public/README.md
@@ -1,6 +1,10 @@
 # Chinese Remainder Theorem Visualization
 
 ## Setting up
-```npm i``
-## Build
+```npm i```
+## Live Development
+```npm run start```
+Then just save your edits and refresh the page to see changes.
+## Or build manually
 ```npm run build```
+Using this method you will need to run this cmd and refresh the page every time you make a change :(
--- public/src/index.js
+++ public/src/index.js
@@ -3,16 +3,30 @@ import Plotly from 'plotly.js-dist'
 window.onload = main;
 
 function main() {
-    const TESTER = document.getElementById('tester');
-    Plotly.newPlot(TESTER,
-        [
+    const graph = new Graph2D(document.getElementById('tester'));
+    graph.draw();
+}
+
+class Graph {
+    constructor(domElement) {
+        this.domElement = domElement;
+    }
+
+    draw() {}
+}
+
+class Graph2D extends Graph {
+    draw() {
+        Plotly.newPlot(this.domElement,
+            [
+                {
+                    x: [1, 2, 3, 4, 5, 6, 7, 8],
+                    y: [1, 2, 3, 4, 5, 6, 7, 8]
+                }
+            ], 
             {
-                x: [1, 2, 3, 4, 5],
-                y: [1, 2, 4, 8, 16]
+                margin: { t: 0 }
             }
-        ], 
-        {
-            margin: { t: 0 }
-        }
-    );
+        );
+    }
 }
